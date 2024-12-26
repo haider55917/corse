e05c556ee418b6be870663bc450d0289ddb24b9a
haider55917 <haider55917@gmail.com>
2024-12-26 11:09:08 +0300

    add new messige

--- get/get/Form1.cs
+++ get/get/Form1.cs
@@ -21,5 +21,10 @@ namespace get
         {
             MessageBox.Show("hhhhh");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("rrrrrr");
+        }
     }
 }
