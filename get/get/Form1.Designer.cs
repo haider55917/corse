f67e5b60cf173e1affced0c10c6f714cf4310c3b
haider55917 <haider55917@gmail.com>
2024-12-27 20:56:50 +0300

    addnew

--- get/get/Form1.Designer.cs
+++ get/get/Form1.Designer.cs
@@ -132,7 +132,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(201, 284);
+            this.button3.Location = new System.Drawing.Point(42, 135);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 10;
