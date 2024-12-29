bc98dddaa7870ee5a4158245f6caffd12e4ebcfd
mwissam <mwissam201@example.com>
2024-12-29 14:18:06 +0300

    fffffff

--- get/get/Form1.Designer.cs
+++ get/get/Form1.Designer.cs
@@ -48,9 +48,10 @@
             // b1
             // 
             this.b1.BackColor = System.Drawing.Color.Silver;
-            this.b1.Location = new System.Drawing.Point(91, 197);
+            this.b1.Location = new System.Drawing.Point(136, 303);
+            this.b1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.b1.Name = "b1";
-            this.b1.Size = new System.Drawing.Size(103, 48);
+            this.b1.Size = new System.Drawing.Size(154, 74);
             this.b1.TabIndex = 0;
             this.b1.Text = "دخول";
             this.b1.UseVisualStyleBackColor = false;
@@ -64,9 +65,11 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.b1);
             this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Location = new System.Drawing.Point(57, 47);
+            this.groupBox1.Location = new System.Drawing.Point(86, 72);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(314, 266);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(471, 409);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -75,36 +78,40 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 15F);
-            this.label2.Location = new System.Drawing.Point(191, 99);
+            this.label2.Location = new System.Drawing.Point(286, 152);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 24);
+            this.label2.Size = new System.Drawing.Size(71, 36);
             this.label2.TabIndex = 3;
             this.label2.Text = "الرمز";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(21, 95);
+            this.textBox2.Location = new System.Drawing.Point(32, 146);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(144, 28);
+            this.textBox2.Size = new System.Drawing.Size(214, 41);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 15F);
-            this.label1.Location = new System.Drawing.Point(191, 49);
+            this.label1.Location = new System.Drawing.Point(286, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 24);
+            this.label1.Size = new System.Drawing.Size(90, 36);
             this.label1.TabIndex = 1;
             this.label1.Text = "الاسم";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(21, 45);
+            this.textBox1.Location = new System.Drawing.Point(32, 69);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(144, 28);
+            this.textBox1.Size = new System.Drawing.Size(214, 41);
             this.textBox1.TabIndex = 0;
             // 
             // b3
@@ -112,9 +119,10 @@
             this.b3.BackColor = System.Drawing.Color.LightGreen;
             this.b3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
             this.b3.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.b3.Location = new System.Drawing.Point(386, 377);
+            this.b3.Location = new System.Drawing.Point(624, 185);
+            this.b3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.b3.Name = "b3";
-            this.b3.Size = new System.Drawing.Size(103, 48);
+            this.b3.Size = new System.Drawing.Size(154, 74);
             this.b3.TabIndex = 4;
             this.b3.Text = "دخول";
             this.b3.UseVisualStyleBackColor = false;
@@ -128,9 +136,11 @@
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.textBox4);
-            this.groupBox2.Location = new System.Drawing.Point(567, 47);
+            this.groupBox2.Location = new System.Drawing.Point(850, 72);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(314, 266);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(471, 409);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -139,36 +149,40 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Tahoma", 15F);
-            this.label3.Location = new System.Drawing.Point(191, 99);
+            this.label3.Location = new System.Drawing.Point(286, 152);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 24);
+            this.label3.Size = new System.Drawing.Size(71, 36);
             this.label3.TabIndex = 3;
             this.label3.Text = "الرمز";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(21, 95);
+            this.textBox3.Location = new System.Drawing.Point(32, 146);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(144, 28);
+            this.textBox3.Size = new System.Drawing.Size(214, 41);
             this.textBox3.TabIndex = 2;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 15F);
-            this.label4.Location = new System.Drawing.Point(191, 49);
+            this.label4.Location = new System.Drawing.Point(286, 75);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 24);
+            this.label4.Size = new System.Drawing.Size(90, 36);
             this.label4.TabIndex = 1;
             this.label4.Text = "الاسم";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(102, 212);
+            this.button1.Location = new System.Drawing.Point(153, 326);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 48);
+            this.button1.Size = new System.Drawing.Size(154, 74);
             this.button1.TabIndex = 0;
             this.button1.Text = "دخول";
             this.button1.UseVisualStyleBackColor = false;
@@ -176,20 +190,22 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(21, 45);
+            this.textBox4.Location = new System.Drawing.Point(32, 69);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(144, 28);
+            this.textBox4.Size = new System.Drawing.Size(214, 41);
             this.textBox4.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 479);
+            this.ClientSize = new System.Drawing.Size(1370, 737);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.b3);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
